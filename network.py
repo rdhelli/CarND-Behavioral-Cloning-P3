716449f3b8cdd69ba729ea05281701844c004357
marton.helli <marton.helli@aimotive.com>
2018-08-03 17:09:57 +0200

    djsaodsa

--- network.py
+++ network.py
@@ -6,7 +6,7 @@ from random import randint
 
 ## OPEN FILES
 samples = []
-with open('./driving_log.csv') as csvfile:
+with open('./data/provided/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
@@ -19,7 +19,7 @@ def generator(samples, batch_size=32):
     num_samples = len(samples)
     correction = 0.2
     while 1:
-        shuffle(samples)
+        sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
             
@@ -28,14 +28,14 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 dir = randint(0,2)
                 flip = randint(0,1)
-                name = './IMG/'+batch_sample[dir].split('\\')[-1]
+                name = './data/provided/IMG/'+batch_sample[dir].split('/')[-1]
                 image = cv2.imread(name)
                 if dir == 0:
                     angle = float(batch_sample[3])
                 if dir == 1:
-                    angle = float(batch_sample[3] + correction)
+                    angle = float(batch_sample[3]) + correction
                 if dir == 2:
-                    angle = float(batch_sample[3] - correction)
+                    angle = float(batch_sample[3]) - correction
                 if flip:
                     image = cv2.flip(image,1)
                     angle *= -1.0
@@ -52,28 +52,30 @@ validation_generator = generator(validation_samples, batch_size=32)
 ## MODEL ARCHITECTURE
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 import matplotlib.pyplot as plt
 
-model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: x / 255.0 - 0.5))
-model.add(Convolution2D(6,(5,5),activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,(5,5),activation='relu'))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
+model = Sequential([
+    Cropping2D(cropping=((60,20), (0,0)), input_shape=(160,320,3)),
+    Lambda(lambda x: x / 255.0 - 0.5),
+    Conv2D(6,(5,5),activation='relu',padding='same'),
+    MaxPooling2D(),
+    Conv2D(12,(5,5),activation='relu',padding='same'),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(120),
+    Dense(84),
+    Dense(1),
+])
+print(model.summary())
+
+for layer in model.layers:
+    print(layer.get_output_at(0).get_shape().as_list())
+
 
 ## MODEL TRAINING AND EVALUATION
 model.compile(loss='mse', optimizer='adam')
-history = model.fit_generator(train_generator,
-                              samples_per_epoch=len(train_samples),
-                              validation_data=validation_generator,
-                              nb_val_samples=len(validation_samples),
-                              nb_epoch=5)
+history = model.fit_generator(train_generator, steps_per_epoch=len(train_samples), validation_data=validation_generator, validation_steps=len(validation_samples), epochs=5, verbose = 1)
 model.save('model.h5')
 print(history.history.keys())
 plt.plot(history.history['loss'])
